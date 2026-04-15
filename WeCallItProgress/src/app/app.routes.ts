c8ad276c27e10b3a213aaf58a241218f3c4d855f
Perido, Maria Cristina T <20162526@s.ubaguio.edu>
2026-04-16 01:31:56 +0800

    Added app routes

--- WeCallItProgress/src/app/app.routes.ts
+++ WeCallItProgress/src/app/app.routes.ts
@@ -1,3 +1,54 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Dashboard } from './dashboard/dashboard';
+import { AdminPanel} from './admin-panel/admin-panel';
+import { EnergyMonitoring } from './energy-monitoring/energy-monitoring';
+import { InfrastructureReporting } from './infrastructure-reporting/infrastructure-reporting';
+import { PublicTransportTracking} from './public-transport-tracking/public-transport-tracking';
+import { SmartTrafficManagement } from './smart-traffic-management/smart-traffic-management';
+import { WasteManagement} from './waste-management/waste-management';
+import { NotFound } from './not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+path: '',
+redirectTo: 'home',
+pathMatch: 'full',
+},
+{
+path: 'home',
+component: Home,
+},
+{
+path: 'dashboard',
+component: Dashboard,
+},
+{
+path: 'admin-panel',
+component: AdminPanel,
+},
+{
+path: 'energy-monitoring',
+component: EnergyMonitoring,
+},
+{
+path: 'infrastructure-reporting',
+component: InfrastructureReporting,
+},
+{
+path: 'public-transport-tracking',
+component: PublicTransportTracking,
+},
+{
+path: 'smart-traffic-management',
+component: SmartTrafficManagement,
+},
+{
+path: 'waste-management',
+component: WasteManagement,
+},
+{
+path: '**',
+component: NotFound,
+},
+];
