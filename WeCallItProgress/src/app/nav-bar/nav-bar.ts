c9affa9269be222944363d8106cdbbd6522c03db
Jan Singilan <singilanjan@gmail.com>
2026-04-16 01:00:11 +0800

    Completed NavBar Component

--- WeCallItProgress/src/app/nav-bar/nav-bar.ts
+++ WeCallItProgress/src/app/nav-bar/nav-bar.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css',
 })
